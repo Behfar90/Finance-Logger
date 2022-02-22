c1cc6bb80012c95782b10084d6547bca545ffabf
Behfar <behfar@timeanddate.com>
2022-02-22 10:49:46 +0100

    First commit

---
+++ public/sandbox.js
@@ -0,0 +1,10 @@
+"use strict";
+let greet = (user) => {
+    console.log(`${user.name} says hello`);
+};
+let add = (a, b, c = 0) => {
+    console.log(a + b);
+    console.log(c);
+};
+let calc;
+cal;
